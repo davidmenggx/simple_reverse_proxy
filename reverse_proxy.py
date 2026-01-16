20ad26ccba5411d0f9f74d29294b2c430cb06bf2
David Meng <davidmenggx@gmail.com>
2026-01-16 13:32:28 -0800

    Temporary change

--- reverse_proxy.py
+++ reverse_proxy.py
@@ -89,6 +89,30 @@ def handle_connection(connection: socket.socket, addr) -> None:
             headers['X-Forwarded-For'] = f'{addr[0].replace("'", '')}'
             headers['X-Forwarded-Proto'] = 'https'
 
+            try:
+                content_length = int(headers.get('content-length', 0)) # important, read this from the headers
+            except ValueError:
+                if VERBOSE: print('Failed to fetch content length')
+                # send back bad request
+
+            body = bytearray(remaining_bytes)
+
+            while len(body) < content_length:
+                bytes_to_read = content_length - len(body)
+                chunk = s.recv(min(bytes_to_read, 4096))
+                if not chunk:
+                    if VERBOSE: print('Failed reading request body')
+                    # send back internal server error
+                    return
+                body.extend(chunk)
+            
+            body = body[:content_length]
+
+            new_request = (f'{method} {path} {protocol_version}\r\n').encode('utf-8')
+            for header in headers:
+                new_request += (f'{header}: {headers[header]}\r\n').encode('utf-8')
+            new_request += b'\r\n' + body
+
             with server_lock:
                 if LOAD_BALANCING_ALGORITHM == 'ROUND_ROBIN':
                     global ROUND_ROBIN_COUNTER
@@ -96,25 +120,87 @@ def handle_connection(connection: socket.socket, addr) -> None:
                 
                 targeted_ip, targeted_port = DISPATCH_DICTIONARY[LOAD_BALANCING_ALGORITHM](servers, addr[0], ROUND_ROBIN_COUNTER) # remember addr is a tuple (host, port, flow info, scope id)
                 servers[(targeted_ip, targeted_port)] += 1
+
+            print(f'chose server ({targeted_ip}, {targeted_port})')
             
             try:
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
                     try:
-                        s.connect((targeted_ip, targeted_port))
+                        s2.connect((targeted_ip, targeted_port))
+                        print(f'connected to chosen server ({targeted_ip}, {targeted_port})')
                     except ConnectionRefusedError:
                         print(f'CRITICAL: Could not connect to server ({targeted_ip}, {targeted_port}), removing from servers list')
                         with server_lock:
                             servers.pop((targeted_ip, targeted_port))
+                        return # return here ????? or try another server ????
 
-                    s.sendall(b'') # IMPORTANT!!! SEE WHAT THIS SHOULD BE
+                    print('sending message to selected server')
 
-                    server_response = ... # THEN WAIT FOR SERVER RESPONSE
+                    s2.sendall(new_request)
+                    print('found response')
+
+
+                    # everything below this is cooked ....
+                    server_buffer = bytearray()
+
+                    while header_delimiter not in server_buffer:
+                        chunk = s2.recv(1024)
+                        # wtf is this figure it out
+                        # if not chunk:
+                        #     if not buffer:
+                        #         if VERBOSE: print('Connection closed cleanly by client')
+                        #         return
+                        #     else:
+                        #         if VERBOSE: print('Connection closed before client sent full header')
+                        #         return
+                        server_buffer.extend(chunk)
+
+                    response_head_raw, _, response_remaining_bytes = server_buffer.partition(header_delimiter) # partition returns (before, delimiter, after)
+                    
+                    response_headers_raw = response_head_raw.decode('utf-8').split('\r\n')[1:]
+
+                    try:
+                        response_headers = get_headers(response_headers_raw)
+                    except ValueError:
+                        if VERBOSE: print('Failed to parse headers from server response')
+                        # send back bad request 400
+                        return
+
+                    #print(response_headers)
+
+                    try:
+                        response_content_length = int(response_headers.get('content-length', 0)) # important, read this from the headers
+                    except ValueError:
+                        if VERBOSE: print('Failed to fetch content length from server response')
+                        # send back bad request
+
+                    response_body = bytearray(response_remaining_bytes)
+
+                    while len(response_body) < response_content_length:
+                        bytes_to_read = response_content_length - len(body)
+                        chunk = s.recv(min(bytes_to_read, 4096))
+                        if not chunk:
+                            if VERBOSE: print('Failed reading request body')
+                            # send back internal server error
+                            return
+                        response_body.extend(chunk)
+                    
+                response_body = response_body[:response_content_length]
+
+                response_message = response_head_raw + header_delimiter + response_body if response_body else response_head_raw + b'\r\n'
+                
+                
+
+
+            # catch exceptions too
             finally:
                 with server_lock:
                     servers[(targeted_ip, targeted_port)] -= 1
                     if servers[(targeted_ip, targeted_port)] < 0: # the # of connections can never be negative
                         print('CRITICAL: # connetions fell below 0. Forcefully resetting back to 0')
                         servers[(targeted_ip, targeted_port)] = 0
+
+
     except OSError as e:
         print(f'SSL error: {e}')
     except Exception as e:
@@ -128,11 +214,14 @@ def discover_servers() -> None:
         discovery_sock.listen() 
 
         while True:
-            _, addr = discovery_sock.accept() # make sure that i don't need to socket itself, just the information
+            conn, addr = discovery_sock.accept() # make sure that i don't need to socket itself, just the information
             # addr is in the form ('IP', PORT, _, _)
+            with conn:
+                data = conn.recv(1024)
+                ip, port = data.decode('utf-8').split(',')
             with server_lock:
-                servers[(addr[0], addr[1])] = 0
-                print(f'found server {addr}')
+                servers[(ip, int(port))] = 0
+                print(f'Found server ({ip}, {port})')
 
 def main() -> None:
     with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as server_sock:
